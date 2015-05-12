cf23649805a27cc5fef713a11b84a259cea3045a
Alex Handy <handy437@gmail.com>
2015-05-12 21:56:19 +0100

    Shots firing

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -3,6 +3,8 @@ require 'battleships'
 
 class BattleshipsApp < Sinatra::Base
 set :views, Proc.new { File.join(root, "..", "views") }
+  @@game = Game.new Player, Board
+  @board = @@game.own_board_view @@game.player_1
 
   get '/' do
     erb :index
@@ -11,6 +13,38 @@ set :views, Proc.new { File.join(root, "..", "views") }
   get '/game/new' do
     erb :new_game
   end
+
+  post '/game' do
+    @name = params[:name]
+    erb :start_game
+  end
+
+  get '/ship/new' do
+    erb :place_ship
+  end
+
+  post '/ship' do
+    #while @@game.player_1.board.ships < 5 do this...
+    @battleship = params[:coordinate1]
+    @@game.player_1.place_ship Ship.battleship, @battleship, :vertically
+    @board = @@game.own_board_view @@game.player_1
+    erb :ship_placed
+    #else do this...
+  end
+
+  get '/shots/new' do
+    erb :take_shots
+  end
+
+  post '/shots' do
+    #while @@game.has_winner? == false
+    @shot = params[:shot]
+    @@game.player_1.shoot @shot.to_sym
+    @opponent_board = @@game.opponent_board_view @@game.player_1
+    erb :view_shots
+    # else include route to winning page
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
