f47ea7a4e36307db96e442b39e662307ad3b1b86
andygout <andygout@hotmail.co.uk>
2015-05-12 18:50:34 +0100

    Furious restart

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -4,9 +4,6 @@ require 'battleships'
 class BattleshipsApp < Sinatra::Base
 set :views, Proc.new { File.join(root, "..", "views") }
 
-  GAME = Game.new Player, Board
-  @@board = GAME.own_board_view GAME.player_1
-
   get '/' do
     erb :index
   end
@@ -15,29 +12,4 @@ set :views, Proc.new { File.join(root, "..", "views") }
     erb :new_game
   end
 
-  post '/game' do
-    p GAME
-    @name = params[:name]
-    @@board = GAME.own_board_view GAME.player_1
-    erb :start_game
-  end
-
-  get '/ship/new' do
-    erb :place_ship
-  end
-
-  post '/ship' do
-    p GAME
-    @battleship = params[:coordinate1]
-    GAME.player_1.place_ship Ship.battleship, @battleship, :vertically
-    @@board = GAME.own_board_view GAME.player_1
-    erb :ship_placed
-  end
-
-  get '/board' do
-
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
 end
